53a61f4eee200d6e43f57cf95b7ad63318445aee
OpheNg <ophe.ng@live.fr>
2023-01-31 21:47:36 +0100

    ask player choice OK, conditions OK, score OK, play again ? OK

--- main.js
+++ main.js
@@ -1,1 +1,124 @@
-main.js
+// function computerPlay() {
+//   let choices = ["rock", "paper", "scissors"];
+//   let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+//   return computerChoice;
+// }
+
+// function playRound(playerSelection, computerSelection) {
+//   playerSelection = playerSelection.toLowerCase();
+//   computerSelection = computerPlay();
+
+//   while (playerSelection !== "rock" &&
+//     playerSelection !== "paper" &&
+//     playerSelection !== "scissors") {
+//       alert('You can only enter : "rock", "paper", or "scissors" !');
+//       playerSelection = prompt("Enter 'rock', 'paper', or 'scissors':").toLowerCase();
+//       playerSelection;
+//   }
+
+//   if (playerSelection === computerSelection) {
+//     return "It's a tie!";
+//   }
+
+//   else if (playerSelection === "rock" && computerSelection === "scissors" ||
+//              playerSelection === "paper" && computerSelection === "rock" ||
+//              playerSelection === "scissors" && computerSelection === "paper") {
+//     return `You Win! ${playerSelection} beats ${computerSelection}`;
+//   }
+//   else {
+//     return `You Lose! ${computerSelection} beats ${playerSelection}`;
+//   }
+// }
+
+// // function game() {
+// //   let score = 0;
+// //   for (let i = 0; i < 5; i++) {
+// //     score += playRound(prompt("Enter :\n- rock\n- paper\n- scissors"));
+// //   }
+// // }
+
+
+// let playerSelection = prompt("What will you chose ? :\n- rock\n- paper\n- scissors");
+// let computerSelection = computerPlay();
+// let result = playRound(playerSelection, computerPlay());
+// console.log(result);
+
+
+// // game();
+// // console.log(score)
+
+
+function computerPlay() {
+  let choices = ["rock", "paper", "scissors"];
+  let computerChoice = choices[Math.floor(Math.random() * choices.length)];
+  return computerChoice;
+}
+
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerPlay();
+
+  while (playerSelection !== "rock" &&
+    playerSelection !== "paper" &&
+    playerSelection !== "scissors") {
+      alert('You can only enter : "rock", "paper", or "scissors" !');
+      playerSelection = prompt("Enter 'rock', 'paper', or 'scissors':").toLowerCase();
+      playerSelection;
+  }
+
+  if (playerSelection === computerSelection) {
+    return "It's a tie!";
+  }
+
+  else if (playerSelection === "rock" && computerSelection === "scissors" ||
+             playerSelection === "paper" && computerSelection === "rock" ||
+             playerSelection === "scissors" && computerSelection === "paper") {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  }
+  else {
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+}
+
+function game() {
+  let score = 0;
+  for (let i = 0; i < 4; i++) {
+    let result = playRound(prompt("Enter 'rock', 'paper', or 'scissors':"));
+    console.log(result);
+    if (result.includes("Win")) {
+      score++;
+    }
+  }
+  if (score >= 3) {
+    console.log(`You win the game! You got a score of ${score}/5`);
+  } else {
+    console.log(`You lost the game! Your score is ${score}/5`);
+  }
+  playAgain();
+}
+
+function playAgain(){
+  let wannaPlay = prompt('Wanna play again ? \nType "yes" or "no"').toLowerCase();
+    if (wannaPlay === "yes"){
+      game();
+    }
+    else if (wannaPlay ==="no"){
+      alert("Goodbye ! See you soon !");
+    }
+    else{
+      alert('You must only enter "yes" or "no".');
+      playAgain();
+    }
+}
+
+
+
+
+let playerSelection = prompt("What will you chose ? :\n- rock\n- paper\n- scissors");
+let computerSelection = computerPlay();
+let result = playRound(playerSelection, computerPlay());
+console.log(result);
+
+
+game();
+console.log(score)
